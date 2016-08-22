87809c0c6aaacb06a3830fa292df39af7a408d8c
James King <james@agentultra.com>
2016-08-22 14:37:14 -0400

    Update aiohttp dependency
    This change updates aiohttp to >= 0.22.1 and updates the code to the new
    APIs.

--- aiocouchdb/hdrs.py
+++ aiocouchdb/hdrs.py
@@ -8,7 +8,7 @@
 #
 
 from aiohttp.hdrs import *
-from aiohttp.multidict import upstr
+from multidict import upstr
 
 #: Defines CouchDB Proxy Auth username
 X_AUTH_COUCHDB_USERNAME = upstr('X-Auth-CouchDB-UserName')
